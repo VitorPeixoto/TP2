cbeff80850262dcce5a50c7476b9883c26db34a1
Vitor Peixoto <peixoto@lsi.cefetmg.br>
2016-06-08 10:39:20 -0300

    Adição das strings dos menus

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,13 @@ import service.PartyService;
 public class Main {
     private static final String[] mainMenu = new String[] {"Módulo do Administrador",
                                                            "Módulo da Eleição"};
+    private static final String[] managerMenu = new String[] {"Parties",
+                                                              "Candidates",
+                                                              "Electors",
+                                                              "Reports",
+                                                              "Main Menu"};
+    private static final String[] electionMenu = new String[] {"Start Election",
+                                                               "Main Menu"};
     public static void main(String[] args) {
         CandidateService candidateService = new CandidateService();
         PartyService     partyService     = new PartyService();
@@ -17,18 +24,16 @@ public class Main {
     }
 
     public static void printMenu(String menuTitle, String[] entries) {
-        int size = menuTitle.length();
-        for (String entry : entries) {
-            if(entry.length() > size) size = entry.length();
-        }
-        size += 6;
-        String middle = (repeat("═", (17*(size)/29)));
+        int size        = 30,
+            spacingSize = size + 18;
+
+        String middle = (repeat("═", size));
 
         System.out.println("╔"+middle+"╗");
-        System.out.println("║"+menuTitle+repeat(" ", size-menuTitle.length())+"║");
+        System.out.println("║"+menuTitle+repeat(" ", spacingSize-menuTitle.length())+"║");
         System.out.println("╠"+middle+"╣");
         for (int i = 0; i < entries.length; i++) {
-            System.out.println("║ "+(i+1)+" - "+entries[i]+repeat(" ", size-entries[i].length()-5)+"║");
+            System.out.println("║ "+(i+1)+" - "+entries[i]+repeat(" ", spacingSize-entries[i].length()-5)+"║");
         }
         System.out.println("╚"+middle+"╝");
         System.out.print("> Opção: ");
