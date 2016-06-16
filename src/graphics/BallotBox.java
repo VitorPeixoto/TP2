6401348abb754a64f11bad1aad7f9ebfa56a3d68
Vítor Peixoto <vitor_peixoto@outlook.com>
2016-06-15 22:37:08 -0300

    Interface

---
+++ src/graphics/BallotBox.java
@@ -0,0 +1,14 @@
+package graphics;
+
+import javax.swing.*;
+
+/**
+ * Created by Peixoto on 15/06/2016.
+ */
+public class BallotBox extends JFrame {
+
+    public BallotBox() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
